2f94aab4237984d794203cb22e64ffa26437d5c4
marco2ice <seanmarcobonifacio@gmail.com>
2026-05-09 09:52:03 +0100

    Add initial frontend routes

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,13 +1,36 @@
+import { BrowserRouter, Routes, Route, NavLink} from "react-router"
+import DashboardPage from "./pages/DashboardPage"
+import CategoriesPage from "./pages/CategoriesPage"
+import BudgetsPage from "./pages/BudgetsPage"
+import TransactionsPage from "./pages/TransactionsPage"
+import RegisterPage from "./pages/RegisterPage"
+import LoginPage from "./pages/LoginPage"
 
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>Personal Budget Tracker</h1>
-      </div>
-    </>
+    <BrowserRouter>
+      {/* Navigation */}
+      <nav>
+        <NavLink to ="/">Dashboard</NavLink> | {" "}
+        <NavLink to = "/categories">Categories</NavLink> | {" "}
+        <NavLink to = "/budgets">Budgets</NavLink> | {" "}
+        <NavLink to = "/transactions">Transactions</NavLink> | {" "}
+        <NavLink to = "/register">Register</NavLink> | {" "}
+        <NavLink to = "/login">Login</NavLink>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path ="/" element={<DashboardPage />} />
+        <Route path ="/categories" element={<CategoriesPage />} />
+        <Route path ="/budgets" element={<BudgetsPage />} />
+        <Route path ="/transactions" element={<TransactionsPage />} />
+        <Route path ="/register" element={<RegisterPage />} />
+        <Route path ="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
